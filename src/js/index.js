dc7b7cf3fb532a4db19f536aacd0521783f67816
V-Yehor <es.valkoff@gmail.com>
2023-10-07 20:30:40 +0300

    Done HW-11

--- src/js/index.js
+++ src/js/index.js
@@ -5,53 +5,58 @@ const form = document.querySelector('#search-form');
 const list = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
 
-const baseUrl = 'https://pixabay.com/api/';
-const API_KEY = '39772594-ca36ede13ec9fedd73f09e8f7';
-let page = 1;
-
 loadBtn.classList.add('ishidden');
 
-form.addEventListener('submit', event => {
-  loadBtn.classList.add('ishidden');
+const baseUrl = 'https://pixabay.com/api/';
+const API_KEY = '39772594-ca36ede13ec9fedd73f09e8f7';
+const per_page = 40;
+let page = 1;
+let searchQuery = '';
+
+form.addEventListener('submit', onFormSubmit);
+loadBtn.addEventListener('click', onLoadBntClick);
+
+async function onFormSubmit(event) {
   event.preventDefault();
-  const serchOpt = event.currentTarget.elements.searchQuery.value;
-  console.log(serchOpt);
-  // form.reset();
-  serchImages(serchOpt);
-});
-
-async function serchImages(serchOpt, page = 1) {
-  try {
-    const response = await axios.get(
-      `${baseUrl}?key=${API_KEY}&q=${serchOpt}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
+  searchQuery = event.target.elements.searchQuery.value.trim();
+  page = 1;
+  list.innerHTML = '';
+  loadBtn.classList.add('ishidden');
+  if (!searchQuery) {
+    loadBtn.classList.add('ishidden');
+    Notiflix.Notify.failure(
+      'The search string cannot be empty. Please specify your search query.'
     );
-    const serchData = response.data;
-    const markup = createMarkup(serchData.hits);
-    const maxHits = serchData.totalHits;
-    console.log(serchData);
-    // console.log(maxHits);
-
-    list.innerHTML = markup;
-    loadBtn.classList.remove('ishidden');
-
-    // loadBtn.addEventListener('click', () => {   //to fix
-    //   page += 1;
-    //   list.insertAdjacentHTML('beforeend', markup);
-    // });
-
-    if (serchData.hits.length === 0) {
-      loadBtn.classList.add('ishidden');
-      Notiflix.Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-  } catch (error) {
-    console.error(error);
+    return;
   }
+  const { hits, totalHits } = await searchImages(searchQuery);
+  loadBtn.classList.remove('ishidden');
+
+  if (hits.length === 0) {
+    loadBtn.classList.add('ishidden');
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+  if (totalHits < per_page) {
+    loadBtn.classList.add('ishidden');
+  } else {
+    loadBtn.classList.remove('ishidden');
+  }
+  Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  renderMarkup(hits);
 }
 
-function createMarkup(serchArr) {
-  return serchArr
+async function searchImages(searchQuery, page = 1) {
+  const { data } = await axios.get(
+    `${baseUrl}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}&page=${page}`
+  );
+  return data;
+}
+
+function renderMarkup(serchArr) {
+  const markup = serchArr
     .map(
       ({
         webformatURL,
@@ -82,4 +87,19 @@ function createMarkup(serchArr) {
       </div>`
     )
     .join('');
+  list.insertAdjacentHTML('beforeend', markup);
+}
+
+async function onLoadBntClick(event) {
+  page += 1;
+  const { hits, totalHits } = await searchImages(searchQuery, page);
+  const totalpage = Math.ceil(totalHits / per_page);
+  renderMarkup(hits);
+
+  if (page > totalpage) {
+    loadBtn.classList.add('ishidden');
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
 }
